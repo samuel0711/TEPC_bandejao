e740787648135928c59a6297a43c32219cae4d1a
Samuel Igor dos Santos Pessoa <samuel.santos1333@gmail.com>
2021-07-18 14:18:18 -0300

    Uso do mongoose para MongoDB (uso da versão atlas em nuvem)

--- bandejao/app.js
+++ bandejao/app.js
@@ -3,9 +3,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var mongoDB = 'mongodb://127.0.0.1/dbbandejao';
+mongoose.connect(mongoDB, { useNewUrlParser: true});
 
 var app = express();
 
@@ -21,6 +24,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
