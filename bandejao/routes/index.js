1cb73e47b7a29ef2e0496638a2e566fd28b70156
Samuel Igor dos Santos Pessoa <samuel.santos1333@gmail.com>
2021-07-18 11:53:08 -0300

    Add nodemon, novo comando pra executar 'npm run devstart' agora

--- bandejao/routes/index.js
+++ bandejao/routes/index.js
@@ -3,8 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Drácula',
-   subtitle: 'A miserable little pile of secrets' });
+  res.render('index', { title: 'Bandejando',
+   subtitle: 'Sistema para alocação de pessoas no bandejão da UFRRJ-IM' });
 });
 
 module.exports = router;
